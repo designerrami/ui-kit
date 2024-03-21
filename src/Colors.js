59ec1d32c36fd5f1286bdb4b823909f65b7e02e8
Ramazan Turan <ramazanturan@gmail.com>
2024-03-21 10:52:40 +0000

    ++

---
+++ src/Colors.js
@@ -0,0 +1,59 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.COLORS = void 0;
+exports.COLORS = {
+    // Primary
+    Workforce1: "#2dccd3",
+    Workforce2: "#59315f",
+    // Secondary
+    Workforce3: "#59315f",
+    Workforce4: "#0097a9",
+    Workforce5: "#00829b",
+    Workforce6: "#006271",
+    WorkforceBackground: "#e0f7f8",
+    WorkforceCard: "#fff",
+    // Greyscale
+    Black: "#000000",
+    Grey1: "#333333",
+    Grey2: "#555555",
+    Grey3: "#7a7a78",
+    Grey4: "#999999",
+    Grey5: "#b2b4b2",
+    Grey6: "#dddddd",
+    Grey7: "#ededed",
+    Grey8: "#fdfefe",
+    Grey9: "#f2f2f3",
+    // Interactive
+    Success: "#4e9b4e",
+    Success2: "#3b763a",
+    Link: "#0d6c98",
+    Warning: "#d7a107",
+    Alert: "#bd362f",
+    // Icons (If there are 1 & 2 variants, colour is a linear gradient)
+    InteractiveIcon1: "#0D6C98",
+    InteractiveIcon2: "#064A69",
+    StaticIcon: "#0D6C98",
+    ActiveSideMenuIcon: "#59315F",
+    InactiveSideMenuIcon: "#00A7B5",
+    HomeWidgetIcon: "#006271",
+    EditIconButton: "#064E70",
+    Divider: "#B2B4B2",
+    ToggleActiveTrack: "#0D6C98",
+    ToggleActiveThumb: "#ffffff",
+    ToggleInactiveTrack: "#ededed",
+    ToggleInactiveThumb: "#ffffff",
+    // Buttons
+    // -- Primary
+    PrimaryBtn1: "#555555",
+    PrimaryBtn2: "#333333",
+    // -- Secondary
+    SecondaryBtn1: "#0D6C98",
+    SecondaryBtn2: "#064A69",
+    // -- Tertiary
+    TertiaryBtnBorder: "#0D6C98",
+    TertiaryBtnText: "#0D6C98",
+    // Other
+    White: "#ffffff",
+    DateInputBorder: "#585858",
+    BorderColor: "#dedede",
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Button = void 0;
+exports.COLORS = exports.Button = void 0;
 var index_1 = require("./Button/index");
 Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return __importDefault(index_1).default; } });
+var Colors_1 = require("./Colors");
+Object.defineProperty(exports, "COLORS", { enumerable: true, get: function () { return Colors_1.COLORS; } });
